739b93b6f4b4878a0849ed3c389b7fae1406c197
Kimgmanis <Kimalexanderkeiser@gmail.com>
2022-03-14 11:43:17 +0100

    Added new user menu

--- src/main/java/com/example/roskildedaycare/AddNewUserController.java
+++ src/main/java/com/example/roskildedaycare/AddNewUserController.java
@@ -10,33 +10,19 @@ import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class UserManagementController implements Initializable {
+public class AddNewUserController implements Initializable {
+
     @FXML
     private Label titleLbl;
     @FXML
-    private Button addnewuserBtn;
-    @FXML
-    private Button existinguserBtn;
-    @FXML
-    private Button edituserBtn;
-    @FXML
-    private Button deleteuserBtn;
-    @FXML
     private Button returnBtn;
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         returnBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                UserSystem.changeScene(event, "admin-logged-in.fxml", " ", null, null);
-            }
-        });
-        addnewuserBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                UserSystem.changeScene(event, "add-new-user.fxml", "Add new user", null, null);
+                UserSystem.changeScene(event, "user-management.fxml", "Log in", null, null);
             }
         });
     }
